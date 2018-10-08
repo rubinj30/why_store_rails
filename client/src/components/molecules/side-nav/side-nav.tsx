d3f9c17ebe1c945588e27784bc466f8432da1e73
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-10-08 18:04:47 -0400

    listing brand names out with all types declared

--- client/src/components/molecules/side-nav/side-nav.tsx
+++ client/src/components/molecules/side-nav/side-nav.tsx
@@ -1,4 +1,37 @@
 import * as React from "react";
-import { SideNavDiv } from './styled';
+import { SideNavDiv } from "./styled";
+import axios from "axios";
 
-export const SideNav = () => <SideNavDiv>SideNav</SideNavDiv>;
+interface IStateInterface {
+  brands: IBrandInterface[];
+}
+
+interface IBrandInterface {
+  name: string;
+  id: number;
+  description: string;
+  image_url: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export class SideNav extends React.Component<{}, IStateInterface> {
+  public state = { brands: [] };
+
+  public componentDidMount() {
+    this.getBrands();
+  }
+
+  public getBrands = async () => {
+    const { data } = await axios.get("/api/brands");
+    this.setState({ brands: data });
+  };
+
+  public render() {
+    const { brands } = this.state;
+    const brandsList = brands.map((brand: IBrandInterface, i) => (
+      <div key={i}>{brand.name}</div>
+    ));
+    return <SideNavDiv>{brandsList}</SideNavDiv>;
+  }
+}
--- client/src/components/molecules/side-nav/styled.tsx
+++ client/src/components/molecules/side-nav/styled.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
-
+// TODO: instead of margin-top use something else
 export const SideNavDiv = styled.div`
      {
         width: 200px;
         height: 80vh;
+        background: red;
+        color: white;
+        margin-top: 45px;
     }
 `;
